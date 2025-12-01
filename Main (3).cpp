ca0bd19af00af9662b77d2c00f5dab7f66ff423b
Kyle VanSaun <kavansaun@ku.edu>
2025-12-01 16:39:26 -0600

    Add files via upload

---
+++ Main (3).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool fail_lecture(const vector<int>& attendance_records) {
+    int absent_count = 0;
+    
+    for (int i = 1; i < attendance_records.size(); ++i) {
+        absent_count += (attendance_records[i] == 0); 
+    }
+    return absent_count >= 3;
+}
+
+// an example main which shows that a student can miss 3 classes but still pass because the first classes attendance doesnt count
+int main() {
+  
+    vector<int> student_attendance = {0, 0, 0, 1, 1, 1, 1, 1, 1, 1};
+    cout << "1 = Fail, 0 = Pass: " << endl;
+    cout << fail_lecture(student_attendance) << endl;
+    
+    return 0;
+}
